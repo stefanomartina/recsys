47b102fe4bbbd414f54e6d257125690338ed5166
Stefano <stefano.martina@mail.polimi.it>
2019-11-18 13:21:34 +0100

    First attempt on new competition (FAILED)

--- testKaggle.py
+++ testKaggle.py
@@ -7,43 +7,61 @@ from recommenders import RandomRecommender as rr
 from utils import extractCSV as e
 
 
-def listToString(s):
-    str1 = ""
-    for ele in s:
-        str1 += " " + str(ele)
-    return str1
+def rowSplit (rowString):
+    split = rowString.split(",")
+    split[3] = split[3].replace("\n", "")
+
+    split[0] = int(split[0])
+    split[1] = int(split[1])
+    split[2] = float(split[2])
+
+    result = tuple(split)
+    return result
+
+
+path_dataset = "/data/recommender-system-2019-challenge-polimi/data_train.csv"
 
 dirname = os.path.dirname(__file__)
-row = np.array(exc.open_csv(dirname + "/data/OLD_DATASET/train.csv"))
+
+row = np.array(exc.open_csv(dirname + path_dataset))
 
 saved_tuple = []
-to_be_printed = []
 
-playlist = row[:, 0][1:]
-song = row[:, 1][1:]
+# user_id = list(row[:, 0][1:])
+# item_id = list(row[:, 1][1:])
 
 index = []
 comma = [","]
 appo = []
 
-playlist_set = set(playlist)
-list_of_ones = np.ones(np.shape(playlist)[0])
-print(np.shape(playlist)[0])
+# user_id_set = set(user_id)
 
-URM_all = sps.coo_matrix((list_of_ones, (playlist, song)), shape=(np.shape(playlist)[0], np.shape(song)[0]))
+userList, itemList, ratings = zip(*row)
+userList_unique = set(userList)
+
+userList = list(userList[1:])
+itemList = list(itemList[1:])
+ratings = list(ratings[1:])
+
+print(str(len(userList)) +" "+ str(len(itemList)) +" "+ str(len(ratings)))
+
+# ones = list(np.ones(len(user_id)))
+
+URM_all = sps.coo_matrix((ratings, (userList, itemList))).tocsr()
 
 randomRecommend = rr.RandomRecommender()
 randomRecommend.fit(URM_all)
 
 
-for i in playlist_set:
+for i in userList_unique:
     index = [str(i)+","]
     appo.clear()
+
     for i in randomRecommend.recommend(i, at=10):
         appo.append(i)
 
     saved_tuple.append(index + appo)
 
-e.write_csv("test.csv", ["playlist_id", "track_ids"], saved_tuple)
+e.write_csv("results/test.csv", ["user_id", "item_list"], saved_tuple)
 
 
