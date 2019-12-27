522d1a9a5ee17134da713854735d1405dd90cfe5
simolanzillotta <simone.lanzillotta@mail.polimi.it>
2019-12-27 14:30:04 +0100

    Small changes

--- ClassesRating.py
+++ ClassesRating.py
@@ -1,6 +1,7 @@
 from Base.BaseFunction import BaseFunction
 import numpy as np
 
+from Hybrid.Hybrid_CB import Hybrid_CB
 from Hybrid.Hybrid_Combo4 import Hybrid_Combo4
 from Hybrid.Hybrid_Combo6_bis import Hybrid_Combo6_bis
 from Hybrid.Hybrid_Combo8 import Hybrid_Combo8
@@ -14,6 +15,7 @@ from Recommenders.MatrixFactorization.PureSVD.PureSVDRecommender import PureSVDR
 from Recommenders.GraphBased.P3AlphaRecommender import P3AlphaRecommender
 from Recommenders.GraphBased.RP3BetaRecommender import RP3BetaRecommender
 from Recommenders.Slim.SlimElasticNet.SLIMElasticNetRecommender import SLIMElasticNetRecommender
+from Recommenders.MatrixFactorization.ALS.ALSRecommender import AlternatingLeastSquare
 from Hybrid.Hybrid_Combo2 import Hybrid_Combo2
 from Utils.evaluation import evaluate_algorithm_classes
 import matplotlib.pyplot as pyplot
@@ -35,7 +37,6 @@ class ClassesRating:
         self.UCM_all = self.helper.UCM_all
         self.initial_target_user = self.helper.userlist_unique
 
-        MAP_TopPop_per_group = []
         MAP_ItemCF_per_group = []
         MAP_UserCF_per_group = []
         MAP_ItemCBF_per_group = []
@@ -49,43 +50,32 @@ class ClassesRating:
         MAP_PureSVD_per_group = []
         MAP_P3Alpha_per_group = []
         MAP_RP3Beta_per_group = []
+        MAP_ALS_per_group = []
         MAP_Hybrid2_per_group = []
         MAP_Hybrid6_per_group = []
         MAP_Hybrid6_bis_per_group = []
         MAP_Hybrid7_per_group = []
         MAP_Hybrid8_per_group = []
+        MAP_HybridCB_per_group = []
 
 
         self.profile_length = np.ediff1d(self.URM_train.indptr)
         self.blocksize = int(len(self.profile_length) * 0.05)
         self.sortedusers = np.argsort(self.profile_length)
 
-        """
-        self.TopPop = TopPopRecommender()
         self.ItemCF = ItemKNNCFRecommender()
         self.UserCF = UserKNNCFRecommender()
         self.ItemCBF = ItemCBFKNNRecommender()
         self.UserCBF = UserCBFKNNRecommender()
-        self.ItemCBF_BM25 = ItemCBFKNNRecommender()
-        self.UserCBF_BM25 = UserCBFKNNRecommender()
-        self.ItemCBF_TFIDF = ItemCBFKNNRecommender()
-        self.UserCBF_TFIDF = UserCBFKNNRecommender()
         self.Slim = SLIM_BPR_Cython()
         self.Elastic = SLIMElasticNetRecommender()
         self.PureSVD = PureSVDRecommender()
         self.P3Alpha = P3AlphaRecommender()
         self.RP3Beta = RP3BetaRecommender()
-        """
-        self.Hybrid2 = Hybrid_Combo2("HybridCombo2", TopPopRecommender())
+        self.ALS = AlternatingLeastSquare()
 
-        self.Hybrid6_bis = Hybrid_Combo6_bis("HybridCombo6_bis", UserCBFKNNRecommender())
-
-        self.Hybrid8 = Hybrid_Combo8("HybridCombo8", TopPopRecommender())
-
-        """
-        self.UserCBF.fit(self.URM_train, self.UCM_all)
         self.ItemCBF.fit(self.URM_train, self.ICM_all)
-        self.TopPop.fit(self.URM_train)
+        self.UserCBF.fit(self.URM_train, self.UCM_all)
         self.ItemCF.fit(self.URM_train)
         self.UserCF.fit(self.URM_train)
         self.Slim.fit(self.URM_train)
@@ -93,18 +83,7 @@ class ClassesRating:
         self.PureSVD.fit(self.URM_train)
         self.P3Alpha.fit(self.URM_train)
         self.RP3Beta.fit(self.URM_train)
-        self.UserCBF_BM25.fit(self.URM_train, self.UCM_all, feature_weighting="BM25")
-        self.ItemCBF_BM25.fit(self.URM_train, self.ICM_all, feature_weighting="BM25")
-        self.UserCBF_TFIDF.fit(self.URM_train, self.UCM_all, feature_weighting="TF-IDF")
-        self.ItemCBF_TFIDF.fit(self.URM_train, self.ICM_all, feature_weighting="TF-IDF")
-        """
-
-        self.Hybrid2.fit(self.URM_train, self.ICM_all, self.UCM_all)
-
-        self.Hybrid8.fit(self.URM_train, self.ICM_all, self.UCM_all)
-
-        self.Hybrid6_bis.fit(self.URM_train, self.ICM_all, self.UCM_all)
-
+        self.ALS.fit(self.URM_train)
 
         for group_id in range(0, 20):
             start_pos = group_id * self.blocksize
@@ -123,27 +102,9 @@ class ClassesRating:
 
             users_in_group = list(set(self.initial_target_user) - set(list(users_not_in_group)))
 
-            """
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.UserCBF, at=10)
-            MAP_UserCBF_per_group.append(results)
 
             results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.ItemCBF, at=10)
             MAP_ItemCBF_per_group.append(results)
-
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.UserCBF_BM25, at=10)
-            MAP_UserCBF_BM25_per_group.append(results)
-
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.ItemCBF_BM25, at=10)
-            MAP_ItemCBF_BM25_per_group.append(results)
-
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.UserCBF_TFIDF, at=10)
-            MAP_UserCBF_TFIDF_per_group.append(results)
-
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.ItemCBF_TFIDF, at=10)
-            MAP_ItemCBF_TFIDF_per_group.append(results)
-
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.TopPop, at=10)
-            MAP_TopPop_per_group.append(results)
 
             results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.ItemCF, at=10)
             MAP_ItemCF_per_group.append(results)
@@ -166,27 +127,15 @@ class ClassesRating:
             results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.RP3Beta, at=10)
             MAP_RP3Beta_per_group.append(results)
 
-            """
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.Hybrid2, at=10)
-            MAP_Hybrid2_per_group.append(results)
+            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.UserCBF, at=10)
+            MAP_UserCBF_per_group.append(results)
+
+            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.ALS, at=10)
+            MAP_ALS_per_group.append(results)
 
 
-
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.Hybrid8, at=10)
-            MAP_Hybrid8_per_group.append(results)
-            
-
-
-            results = evaluate_algorithm_classes(self.URM_test, users_in_group, self.Hybrid6_bis, at=10)
-            MAP_Hybrid6_bis_per_group.append(results)
-
-        """
         pyplot.plot(MAP_UserCBF_per_group, label="UserCBF")
         pyplot.plot(MAP_ItemCBF_per_group, label="ItemCBF")
-        pyplot.plot(MAP_UserCBF_BM25_per_group, label="UserCBF_BM25")
-        pyplot.plot(MAP_ItemCBF_BM25_per_group, label="ItemCBF_BM25")
-        pyplot.plot(MAP_UserCBF_TFIDF_per_group, label="UserCBF_TFIDF")
-        pyplot.plot(MAP_ItemCBF_TFIDF_per_group, label="ItemCBF_TFIDF")
         pyplot.plot(MAP_ItemCF_per_group, label="ItemCF")
         pyplot.plot(MAP_UserCF_per_group, label="UserCF")
         pyplot.plot(MAP_Slim_per_group, label="Slim")
@@ -194,18 +143,14 @@ class ClassesRating:
         pyplot.plot(MAP_P3Alpha_per_group, label="P3Alpha")
         pyplot.plot(MAP_RP3Beta_per_group, label="RP3Beta")
         pyplot.plot(MAP_PureSVD_per_group, label="PureSVD")
-        pyplot.plot(MAP_TopPop_per_group, label="TopPop")
-        """
-        pyplot.plot(MAP_Hybrid2_per_group, label="Hybrid2")
-        pyplot.plot(MAP_Hybrid7_per_group, label="Hybrid7")
-        pyplot.plot(MAP_Hybrid8_per_group, label="Hybrid8")
-        pyplot.plot(MAP_Hybrid6_per_group, label="UserCBF")
-        pyplot.plot(MAP_Hybrid6_bis_per_group, label="UserCBF")
+        pyplot.plot(MAP_UserCBF_per_group, label="UserCBF")
+        pyplot.plot(MAP_ALS_per_group, label="ALS")
 
-
-        pyplot.ylabel('MAP')
         pyplot.xlabel('User Group')
-        pyplot.legend()
+        pyplot.ylabel('MAP')
+        pyplot.xticks(np.arange(0, 20, 1))
+        pyplot.grid(b=True, axis='both', color='firebrick', linestyle='--', linewidth=0.5)
+        pyplot.legend(loc='lower right')
         pyplot.show()
 
 
@@ -247,4 +192,4 @@ class LongTail:
 
 
 if __name__ == '__main__':
-    stats = LongTail()
+    stats = ClassesRating()
